80601ac1d3b9ba9a69783df2ad17b8d590322efa
unknown <hanohj@gmail.com>
2020-05-18 13:56:28 +0300

    Added Styling

--- src/components/DrugTable.js
+++ src/components/DrugTable.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -9,15 +10,54 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import axios from 'axios';
 
-const useStyles = makeStyles({
+const styles = (theme) => ({
   table: {
     minWidth: 650,
+    width: '50%',
+  },
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  tableRow: {
+    '&$selected, &$selected:hover': {
+      backgroundColor: 'purple',
+    },
+  },
+  tableCell: {
+    '$selected &': {
+      color: 'yellow',
+    },
+  },
+  flexContainer: {
+    height: '100%',
+    padding: 0,
+    margin: 0,
+    // display: -webkit-box ,
+    // display: -moz-box ,
+    // display: -ms-flexbox ,
+    // display: -webkit-flex ,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  flexItem: {
+    backgroundColor: 'lightblue',
+    padding: '5px',
+    width: '20px',
+    height: '20px',
+    margin: '10px',
+    lineHeight: '20px',
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: '2em',
+    textAlign: 'center',
   },
 });
 
-const SimpleTable = ({term}) => {
+const SimpleTable = ({term, classes}) => {
   const [drugs, setDrugs] = useState([]);
-
   useEffect(() => {
     async function fetchData() {
       await fetchDrugs();
@@ -39,24 +79,33 @@ const SimpleTable = ({term}) => {
     }
   };
 
-  const classes = useStyles();
-
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
+    <TableContainer component={Paper} className={classes.flexContainer}>
+      <Table
+        className={`${classes.table} ${classes.flexItem}`}
+        aria-label="simple table"
+      >
         <TableHead>
-          <TableRow>
-            <TableCell>Term</TableCell>
-            <TableCell align="right">Count</TableCell>
+          <TableRow hover className={classes.tableRow}>
+            <TableCell className={classes.tableCell}>Term</TableCell>
+            <TableCell className={classes.tableCell} align="right">
+              Count
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {drugs.map((row) => (
-            <TableRow key={row.term}>
-              <TableCell component="th" scope="row">
+            <TableRow key={row.term} hover className={classes.tableRow}>
+              <TableCell
+                className={classes.tableCell}
+                component="th"
+                scope="row"
+              >
                 {row.term}
               </TableCell>
-              <TableCell align="right">{row.count}</TableCell>
+              <TableCell className={classes.tableCell} align="right">
+                {row.count}
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -65,4 +114,4 @@ const SimpleTable = ({term}) => {
   );
 };
 
-export default SimpleTable;
+export default withStyles(styles)(SimpleTable);
